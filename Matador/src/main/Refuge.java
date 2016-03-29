451fb5f62f1dc78b6ba649af6a6861125f83e50b
Frederik <frederiks96@gmail.com>
2016-03-29 14:04:38 +0200

    .

--- Matador/src/main/Refuge.java
+++ Matador/src/main/Refuge.java
@@ -4,7 +4,6 @@ public class Refuge extends AbstractFields {
 
 	@Override
 	public int getRent(int id) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
--- Matador/src/main/Tax.java
+++ Matador/src/main/Tax.java
@@ -4,7 +4,6 @@ public class Tax extends AbstractFields {
 
 	@Override
 	public int getRent(int id) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
