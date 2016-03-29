c3c10c59aa35ae6ae5f8011a2067163e4884ff03
Frederik <frederiks96@gmail.com>
2016-03-29 14:25:25 +0200

    Opdateret Fleet

--- Matador/src/main/Fleet.java
+++ Matador/src/main/Fleet.java
@@ -3,16 +3,16 @@ package main;
 public class Fleet extends AbstractOwnable {
 	
 	private int id;
-	private int baserent = 500;
+	private final int BASERENT = 500;
 	private Player owner;
 	
 	public Fleet(int id) {
 		this.id=id;
-		this.owner=null; // Skal være brættet/banken
+		owner=null; // Skal være brættet/banken
 	}
 	
 	public int getRent(int id) {
-		return (int) Math.pow(baserent, owner.getFleets());
+		return (int) Math.pow(BASERENT, owner.getFleets());
 	}
 
 }
