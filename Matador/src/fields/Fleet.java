32a24c8afc330376403b2bdc7a69e657a10e28f1
Oliver <Oliver.e.t@hotmail.com>
2016-03-29 15:06:45 +0200

    rename

---
+++ Matador/src/fields/Fleet.java
@@ -0,0 +1,20 @@
+package fields;
+
+import main.AbstractOwnable;
+import main.Player;
+
+public class Fleet extends AbstractOwnable {
+	
+	private final int BASERENT = 500;
+	private Player owner;
+	
+	public Fleet(int id) {
+		super(id);
+		owner=null; // Skal være brættet/banken
+	}
+	
+	public int getRent(int id) {
+		return (int) Math.pow(BASERENT, owner.getFleets());
+	}
+
+}
---
+++ Matador/src/fields/Territory.java
@@ -0,0 +1,15 @@
+package fields;
+
+import main.AbstractOwnable;
+
+public class Territory extends AbstractOwnable {
+	
+	public Territory(int id) {
+		super(id);
+	}
+
+	public int getRent(int id) {
+		return 0;
+	}
+
+}
