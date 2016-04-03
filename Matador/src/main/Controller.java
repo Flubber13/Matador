d9cece38f278afb52469d360f441e6ba185d4623
Frederik <frederiks96@gmail.com>
2016-04-03 11:37:23 +0200

    Lavet Controller interface, så man har en "opskrift", i det omfang, at controlleren skal udskiftes. Så kan man udskifte GUI'en, og controlleren, men beholde den samme kode i alle andre klasser, da metoderne i controlleren kommer til at hedde det samme, men metoderne i GUI'en varierer, og det ændrer man så bare i controlleren

--- Matador/src/main/Controller.java
+++ Matador/src/main/Controller.java
@@ -1,5 +1,41 @@
 package main;
 
-public class Controller {
+import desktop_codebehind.Car;
+import desktop_resources.GUI;
+
+public class Controller implements IController {
+		
+	public String getUserString(String message) {
+		return GUI.getUserString(message);
+	}
+	
+	public void addPlayer(String name, int balance) {
+		GUI.addPlayer(name, balance);
+	}
+	
+	public void addPlayer(String name, int balance, Car car) {
+		GUI.addPlayer(name, balance, car);
+	}
+
+	public String getUserButtonPressed(String message, String... buttons) {
+		return GUI.getUserButtonPressed(message, buttons);
+	}
+
+	public int getUserInteger(String message, int min, int max) {
+		return GUI.getUserInteger(message, min, max);
+	}
+
+	public int getUserInteger(String message) {
+		return GUI.getUserInteger(message);
+	}
+
+	public String getUserSelection(String message, String... options) {
+		return GUI.getUserSelection(message, options);
+	}
+
+	public boolean getUserLeftButtonPressed(String message, String trueButton, String falseButton) {
+		return GUI.getUserLeftButtonPressed(message, trueButton, falseButton);
+	}
+	
 
 }
---
+++ Matador/src/main/IController.java
@@ -0,0 +1,20 @@
+package main;
+
+import desktop_codebehind.Car;
+
+public interface IController {
+	
+	// User interference
+	
+	String getUserString(String message);
+	String getUserButtonPressed(String message, String... buttons);
+	int getUserInteger(String message, int min, int max); // Kan udelades
+	int getUserInteger(String message);
+	String getUserSelection(String message, String... options);
+	boolean getUserLeftButtonPressed(String message, String trueButton, String falseButton);
+	
+	// GUI relateret
+	void addPlayer(String name, int balance);
+	void addPlayer(String name, int balance, Car car);
+
+}
