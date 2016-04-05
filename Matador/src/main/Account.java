fd19c793ae08abb5a7a660b8760ca0dd7b933ae0
Frederik <frederiks96@gmail.com>
2016-04-05 15:34:03 +0200

    Lavet account klasse

---
+++ Matador/src/main/Account.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Account {
+	
+	private int balance;
+	
+	
+	public Account() {
+		this.balance = 30000;
+	}
+	
+	public int getBalance() {
+		return this.balance;
+	}
+	
+	public void updateBalance(int newBalance) {
+		this.balance = newBalance;
+	}
+
+}
