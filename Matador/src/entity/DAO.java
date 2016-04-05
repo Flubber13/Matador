8747129b750cdfd62408e211e67a7ea029f5215c
Frederik <frederiks96@gmail.com>
2016-04-05 14:51:12 +0200

    Tilføjet DAO interface

---
+++ Matador/src/entity/DAO.java
@@ -0,0 +1,21 @@
+package entity;
+
+public interface DAO {
+	
+	// Data Acces object - setters
+	
+	int getPosition();
+	int getBalance();
+	int getJailTime();
+	int getCardId();
+	int getCardPosition();
+	int getFieldId();
+	int getHouseCount();
+	boolean hasHotel();
+	boolean isMortgaged();
+	int getVehicleID();
+	String getVehicleColour();
+	String getVehicleType();
+	int getAccountId();
+
+}
