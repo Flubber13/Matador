f2b0a3b2680d8db085c4e7dbcec358a870450a49
Frederik <frederiks96@gmail.com>
2016-04-12 11:49:44 +0200

    SQL klasse

---
+++ Matador/src/entity/SQL.java
@@ -0,0 +1,135 @@
+package entity;
+
+public class SQL implements DAO, DTO {
+
+	public int getPosition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getBalance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getJailTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getCardId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getCardPosition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getFieldId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHouseCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean hasHotel() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isMortgaged() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int getVehicleID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getVehicleColour() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getVehicleType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getAccountId() {
+		// TODO Auto-generated method stub
+		return 0;
+	} 
+
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setBalance() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setJailTime() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setCardId() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setCardPosition() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setFieldId() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setHouseCount() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void buildHotel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void mortgage() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setVehicleID() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setVehicleColour() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setVehicleType() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setAccountId() {
+		// TODO Auto-generated method stub
+
+	}
+}
